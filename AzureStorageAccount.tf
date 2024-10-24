8abb505608dab9f9a739505a7ad888f509a97039
Einfochips@1234 <yakshbdm02@gmail.com>
2024-10-24 15:11:47 +0530

    terra gitaction

--- AzureStorageAccount.tf
+++ AzureStorageAccount.tf
@@ -2,11 +2,10 @@ data "azurerm_resource_group" "demo" {
     name = "sa1_test_eic_YakshRawal"
 }
 
-##  Demo now
 resource "azurerm_storage_account" "StorageAccountDemo" {
   name                     = "resource123"
-  resource_group_name      = azurerm_resource_group.demo.name
-  location                 = azurerm_resource_group.demo.location
+  resource_group_name      = data.azurerm_resource_group.demo.name
+  location                 = data.azurerm_resource_group.demo.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
