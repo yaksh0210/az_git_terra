992fe9c53126087434809774502328dfe39a86c8
Einfochips@1234 <yakshbdm02@gmail.com>
2024-10-24 15:01:55 +0530

    terra gitaction

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+cloud {
+    organization = "yakshcloud"
+    workspaces {
+      name = "myworkspace"
+    }
+  }
+}
+
 provider "azurerm" {
   subscription_id                 = "664b6097-19f2-42a3-be95-a4a6b4069f6b"
   resource_provider_registrations = "none"
