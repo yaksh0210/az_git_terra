9ddfd2db0e6c5a6073aaacc6fcd8a0eda6f676fd
Einfochips@1234 <yakshbdm02@gmail.com>
2024-10-24 15:03:46 +0530

    terra gitaction

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ cloud {
 
 provider "azurerm" {
   subscription_id                 = "664b6097-19f2-42a3-be95-a4a6b4069f6b"
-  resource_provider_registrations = "none"
+#   resource_provider_registrations = "none"
   features {}
 }
 
